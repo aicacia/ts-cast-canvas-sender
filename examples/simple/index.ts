5e4c80622025e54254a9a3c67f9435c63985c5c9
Nathan Faucett <nathanfaucett@gmail.com>
2020-08-23 11:08:28 -0400

    clean up

--- examples/simple/index.ts
+++ examples/simple/index.ts
@@ -1,63 +1,67 @@
-import { events, sendMediaStream } from "../../lib";
+import { events, sendMediaStream } from "../../src";
 
-let video: HTMLVideoElement = null as any,
-  canvas: HTMLCanvasElement = null as any,
-  ctx: CanvasRenderingContext2D = null as any,
-  mediaStream: MediaStream = null as any;
+let video: HTMLVideoElement | undefined,
+  // canvas: HTMLCanvasElement | undefined,
+  // ctx: CanvasRenderingContext2D | undefined,
+  mediaStream: MediaStream | undefined;
 
-function loop(ms: number) {
-  const s = ms * 0.001,
-    hw = canvas.width * 0.5,
-    hh = canvas.height * 0.5,
-    x = hw + hw * 0.5 * Math.cos(s),
-    y = hh + hh * 0.5 * Math.sin(s);
+// function loop(ms: number) {
+//   const s = ms * 0.001,
+//     hw = canvas.width * 0.5,
+//     hh = canvas.height * 0.5,
+//     x = hw + hw * 0.5 * Math.cos(s),
+//     y = hh + hh * 0.5 * Math.sin(s);
 
-  ctx.save();
-  ctx.fillStyle = "rgba(150, 150, 150, 0.1)";
-  ctx.fillRect(0, 0, canvas.width, canvas.height);
-  ctx.restore();
+//   ctx.save();
+//   ctx.fillStyle = "rgba(150, 150, 150, 0.1)";
+//   ctx.fillRect(0, 0, canvas.width, canvas.height);
+//   ctx.restore();
 
-  ctx.save();
-  ctx.beginPath();
-  ctx.fillStyle = "#333";
-  ctx.arc(x, y, 64, 0, 2 * Math.PI, false);
-  ctx.fill();
-  ctx.restore();
+//   ctx.save();
+//   ctx.beginPath();
+//   ctx.fillStyle = "#333";
+//   ctx.arc(x, y, 64, 0, 2 * Math.PI, false);
+//   ctx.fill();
+//   ctx.restore();
 
-  window.requestAnimationFrame(loop);
-}
+//   window.requestAnimationFrame(loop);
+// }
 
 function onStart() {
-  sendMediaStream(mediaStream);
+  if (mediaStream) {
+    sendMediaStream(mediaStream);
+  }
 }
 
-function onEnd() {}
+function onEnd() {
+  return undefined;
+}
 
 function onLoad() {
-  video = document.createElement("video") as HTMLVideoElement;
-  video.setAttribute("playsinline", "true");
-  video.setAttribute("autoplay", "true");
-  canvas = document.createElement("canvas");
-  ctx = canvas.getContext("2d") as CanvasRenderingContext2D;
+  video = document.getElementById("video") as HTMLVideoElement;
+  // canvas = document.getElementById("canvas") as HTMLCanvasElement;
+  // ctx = canvas.getContext("2d") as CanvasRenderingContext2D;
 
-  mediaStream = (canvas as any).captureStream(60);
-  video.srcObject = mediaStream;
+  // mediaStream = (canvas as any).captureStream(60);
+  // video.srcObject = mediaStream;
 
-  // navigator.mediaDevices
-  //   .getUserMedia({ video: true, audio: false })
-  //   .then(stream => {
-  //     mediaStream = stream;
-  //     video.srcObject = mediaStream;
-  //   });
+  navigator.mediaDevices
+    .getUserMedia({ video: true, audio: true })
+    .then((stream) => {
+      mediaStream = stream;
+      video.srcObject = mediaStream;
+    });
 
   onResize(window.innerWidth, window.innerHeight);
 
-  window.requestAnimationFrame(loop);
+  // window.requestAnimationFrame(loop);
 }
 
-function onResize(width: number, height: number) {
-  canvas.width = width;
-  canvas.height = height;
+function onResize(_width: number, _height: number) {
+  // if (canvas) {
+  //   canvas.width = width;
+  //   canvas.height = height;
+  // }
 }
 
 events.on("start", onStart);
--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,15 @@
 import { none, Option } from "@aicacia/core";
+import { IJSONObject } from "@aicacia/json";
 import { EventEmitter } from "events";
 
 const NAMSPACE = "urn:x-cast:aicacia",
-  STUN_URL = "stun:stun.l.google.com:19302";
+  STUN_URL = "stun:stun.l.google.com:19302",
+  RECEIVER_APPLICATION_ID = "41A895CA";
 
 export const events = new EventEmitter();
 
 const PEER_CONNECTION: Option<RTCPeerConnection> = none();
-const RTP_SENDER: Option<RTCRtpSender> = none();
+const RTP_SENDERS: Map<any, RTCRtpSender> = new Map();
 
 const onGCastApiAvailable = (available: boolean, reason: string) => {
   if (available) {
@@ -27,38 +29,43 @@ function getCurrentSession(): Option<cast.framework.CastSession> {
   );
 }
 
-function initializeCastApi() {
+let castApiInitialized = false;
+
+export function initializeCastApi() {
+  if (castApiInitialized) {
+    return;
+  } else {
+    castApiInitialized = true;
+  }
   const castContext = cast.framework.CastContext.getInstance();
 
   castContext.setOptions({
-    receiverApplicationId: "41A895CA",
-    autoJoinPolicy: chrome.cast.AutoJoinPolicy.ORIGIN_SCOPED
+    receiverApplicationId: RECEIVER_APPLICATION_ID,
+    autoJoinPolicy: chrome.cast.AutoJoinPolicy.ORIGIN_SCOPED,
   });
 
   castContext.addEventListener(
     cast.framework.CastContextEventType.SESSION_STATE_CHANGED,
-    sessionEvent => {
+    (sessionEvent) => {
       switch (sessionEvent.sessionState) {
         case cast.framework.SessionState.SESSION_STARTED: {
           PEER_CONNECTION.replace(
             new RTCPeerConnection({
               iceServers: [
                 {
-                  urls: STUN_URL
-                }
-              ]
+                  urls: STUN_URL,
+                },
+              ],
             })
-          ).ifSome(pc => {
-            pc.addEventListener("icecandidate", event => {
+          ).ifSome((pc) => {
+            pc.addEventListener("icecandidate", (event) => {
               if (event.candidate) {
-                getCurrentSession().ifSome(session => {
-                  session
-                    .sendMessage(NAMSPACE, {
-                      type: "candidate",
-                      candidate: event.candidate as any
-                    })
-                    .catch(createOnError("icecandidate"));
-                });
+                sendMessage({
+                  type: "candidate",
+                  candidate: event.candidate as any,
+                }).ifSome((promise) =>
+                  promise.catch(createOnError("icecandidate"))
+                );
               }
             });
 
@@ -69,18 +76,18 @@ function initializeCastApi() {
             });
           });
 
-          getCurrentSession().ifSome(session => {
+          getCurrentSession().ifSome((session) => {
             session.addMessageListener(NAMSPACE, (namespace, json) => {
               const event = JSON.parse(json);
 
               switch (event.type) {
                 case "sdp":
-                  PEER_CONNECTION.ifSome(pc => {
+                  PEER_CONNECTION.ifSome((pc) => {
                     pc.setRemoteDescription(
-                      new RTCSessionDescription(event.sdp as any)
+                      new RTCSessionDescription(event.sdp)
                     )
                       .then(() => {
-                        PEER_CONNECTION.ifSome(pc => {
+                        PEER_CONNECTION.ifSome((pc) => {
                           if (pc.remoteDescription?.type === "offer") {
                             pc.createAnswer()
                               .then(onLocalDescCreated)
@@ -92,10 +99,10 @@ function initializeCastApi() {
                   });
                   break;
                 case "candidate":
-                  PEER_CONNECTION.ifSome(pc => {
-                    pc.addIceCandidate(new RTCIceCandidate(event.candidate))
-                      .then(() => {})
-                      .catch(createOnError("addIceCandidate"));
+                  PEER_CONNECTION.ifSome((pc) => {
+                    pc.addIceCandidate(
+                      new RTCIceCandidate(event.candidate)
+                    ).catch(createOnError("addIceCandidate"));
                   });
                   break;
                 case "resize":
@@ -109,7 +116,7 @@ function initializeCastApi() {
           break;
         }
         case cast.framework.SessionState.SESSION_ENDED: {
-          removeMediaStream();
+          removeMediaStreamTracks();
           events.emit("end");
           break;
         }
@@ -119,35 +126,56 @@ function initializeCastApi() {
 }
 
 export function sendMediaStream(mediaStream: MediaStream) {
-  removeMediaStream();
-  PEER_CONNECTION.ifSome(pc => {
-    RTP_SENDER.replace(
-      pc.addTrack(mediaStream.getVideoTracks()[0], mediaStream)
-    );
+  PEER_CONNECTION.ifSome((pc) => {
+    mediaStream.getAudioTracks().forEach((audioTrack) => {
+      RTP_SENDERS.set(audioTrack, pc.addTrack(audioTrack, mediaStream));
+    });
+    mediaStream.getVideoTracks().forEach((videoTrack) => {
+      RTP_SENDERS.set(videoTrack, pc.addTrack(videoTrack, mediaStream));
+    });
   });
 }
 
-export function removeMediaStream() {
-  RTP_SENDER.take().ifSome(sender => {
-    PEER_CONNECTION.ifSome(pc => {
+export function removeMediaStream(mediaStream: MediaStream) {
+  mediaStream.getAudioTracks().forEach(removeMediaStreamTrack);
+  mediaStream.getVideoTracks().forEach(removeMediaStreamTrack);
+}
+
+export function removeMediaStreamTrack(mediaStreamTrack: MediaStreamTrack) {
+  PEER_CONNECTION.ifSome((pc) => {
+    Option.from(RTP_SENDERS.get(mediaStreamTrack)).ifSome((sender) => {
+      pc.removeTrack(sender);
+      RTP_SENDERS.delete(mediaStreamTrack);
+    });
+  });
+}
+
+export function removeMediaStreamTracks() {
+  PEER_CONNECTION.ifSome((pc) => {
+    RTP_SENDERS.forEach((sender) => {
       pc.removeTrack(sender);
     });
   });
+  RTP_SENDERS.clear();
 }
 
 function onLocalDescCreated(desc: RTCSessionDescriptionInit) {
-  return PEER_CONNECTION.ifSome(pc => {
+  return PEER_CONNECTION.ifSome((pc) => {
     pc.setLocalDescription(desc).then(() => {
-      getCurrentSession().ifSome(session => {
-        session
-          .sendMessage(NAMSPACE, {
-            type: "sdp",
-            sdp: pc.localDescription
-          })
-          .catch(createOnError("setLocalDescription"));
-      });
+      sendMessage({
+        type: "sdp",
+        sdp: pc.localDescription as any,
+      }).ifSome((promise) =>
+        promise.catch(createOnError("setLocalDescription"))
+      );
     });
   });
+}
+
+function sendMessage(message: IJSONObject) {
+  return getCurrentSession().map((session) =>
+    session.sendMessage(NAMSPACE, message)
+  );
 }
 
 function createOnError(name: string) {
